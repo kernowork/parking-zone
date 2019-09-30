83219406848e88ffc04471b5bc67c201b48e1394
kernowork <14323418+kernowork@users.noreply.github.com>
2019-09-30 13:07:04 -0700

    minor edit

--- CustomFields/aura/CustomFieldsHelper.js
+++ CustomFields/aura/CustomFieldsHelper.js
@@ -22,9 +22,7 @@
             if(response.getState() === "SUCCESS") {
              console.log('Found fields!'); 
              var result = response.getReturnValue();
-             console.log(result);
              component.set("v.customFields", result); 
-             console.log(component.get("v.customFields"));
              component.set("v.loading", false);
             } else {
                 console.log('NO GOOD!');
@@ -34,19 +32,4 @@
         $A.enqueueAction(action);
     },
 
-    navigateToRecord: function(component, recId) {
-        var navService = component.find("navService");
-        
-        var pageReference = {
-            type: 'standard__recordPage',
-            attributes: {
-                "recordId": recId,
-                "objectApiName": "Workshop__c",
-                "actionName": "view"
-            }
-        }
-        
-        navService.navigate(pageReference); 
-        $A.get('e.force:refreshView').fire();
-    },
 })
